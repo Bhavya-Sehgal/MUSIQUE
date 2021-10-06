d8c76f75fce90ba82c2cd1942ca1f26327b2b4ac
bhavya-sehgal <bhavyasehgal05@gmail.com>
2021-10-06 13:34:40 +0530

    get token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  // run code based on given condition
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", _token);
+  }, [token]);
+
   return (
-    <div className="app">
-      <Login />
-    </div>
+    // conditional rendering
+    <div className="app">{token ? <h1>Logged In</h1> : <Login />}</div>
   );
 }
 
---
+++ src/spotify.js
@@ -0,0 +1,35 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "8fff83ff78804c4ab477ceb760c12f89";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+// scopes include functionalities/permissions for user
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+
+// console.log(getTokenFromUrl());
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
+
+// this token will be generated after authentication
+// show dialog will generate popup prompt
